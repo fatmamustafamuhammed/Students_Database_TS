fdbda9553c9bfb6a681e8552c7bdfe920157f613
fatmamustafamuhammed <fmomen738@gmail.com>
2025-08-16 17:24:24 +0300

    Students Database

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+import data from './data.js';
+const students = JSON.parse(data);
+function addRow(table, student) {
+    let tr = table.querySelector("tbody").insertRow();
+    const name = tr.insertCell();
+    name.appendChild(document.createTextNode(`${student.firstName}  ${student.lastName}`));
+    const age = tr.insertCell();
+    age.appendChild(document.createTextNode((new Date().getFullYear() - parseInt(student.birthYear)).toString()));
+    const majors = tr.insertCell();
+    if (student.focusArea) {
+        if (typeof student.focusArea == "string") {
+            majors.appendChild(document.createTextNode(student.focusArea));
+        }
+        else {
+            let areas = student.focusArea.join(", ");
+            majors.appendChild(document.createTextNode(areas));
+        }
+    }
+    else {
+        majors.appendChild(document.createTextNode("--"));
+    }
+    const status = tr.insertCell();
+    if (student.dateRegistrationSuspended) {
+        status.appendChild(document.createTextNode("Inactive"));
+    }
+    else {
+        status.appendChild(document.createTextNode("Active"));
+    }
+}
+function selectTable() {
+    return document.querySelector("#students-table");
+}
+function refreshTable(table, students) {
+    table.querySelector("tbody").innerHTML = "";
+    students.forEach(student => {
+        addRow(table, student);
+    });
+}
+window.onload = function () {
+    refreshTable(selectTable(), students);
+};
+//# sourceMappingURL=index.js.map
